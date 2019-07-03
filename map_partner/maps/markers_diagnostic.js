9a6d8879922f6672885a04306150c658f85decee
sifat <hello_sifat@yahoo.com>
2019-07-03 14:24:13 +0600

    map_partner added

---
+++ map_partner/maps/markers_diagnostic.js
@@ -0,0 +1,91 @@
+var markers_diagnostic = [
+  {
+    "name":"IBN Sina D.Lab & Consultation Centre",
+    "city":"ঢাকা",
+    "phone":"01945329976",
+    "partnercode":"AYG01",
+    "lat":23.7106169,
+    "lng":90.4224933,
+    "address":"sample_address",
+    "discount_info" : "20% on pathology",
+    "tz":"Asia/Dhaka"
+  },
+  {
+    "name":"Chittagong belle vue ltd",
+    "city":"Chittagong",
+    "phone":"01945329445",
+    "partnercode":"AYM05",
+    "lat":22.3540978,
+    "lng":91.8201855,
+    "address":"sample_address",
+    "discount_info" : "30% on radiology",
+    "tz":"Asia/Dhaka"
+  },
+  {
+    "name":"Evolution Dental Chamber, Haji Mohasin Road, Khulna",
+    "city":"Khulna",
+    "phone":"01945329921",
+    "partnercode":"KDL02",
+    "lat":22.8081275,
+    "lng":89.5672976,
+    "address":"sample_address",
+    "discount_info" : "25% on pathology",
+    "tz":"Asia/Dhaka"
+  },
+  {
+    "name":"Fair Diagnostic Centre",
+    "city":"Rangpur",
+    "phone":"019453299XX",
+    "partnercode":"FDRD1",
+    "lat":25.7634082,
+    "lng":89.232399,
+    "address":"sample_address",
+    "discount_info" : "25% on pathology",
+    "tz":"Asia/Dhaka"
+  },
+  {
+    "name":"Popular Diagnostic Centre",
+    "city":"Rajshahi",
+    "phone":"019453299XX",
+    "partnercode":"PDC15",
+    "lat":25.7634082,
+    "lng":89.232399,
+    "address":"sample_address",
+    "discount_info" : "12% on pathology",
+    "tz":"Asia/Dhaka"
+  },
+  {
+    "name":"Popular Diagnostic Centre",
+    "city":"Dinajpur",
+    "phone":"019453299XX",
+    "partnercode":"PDC19",
+    "lat":25.6200327,
+    "lng":88.6428827,
+    "address":"sample_address",
+    "discount_info" : "18% on pathology",
+    "tz":"Asia/Dhaka"
+  },
+  {
+    "name":"Pure Scientific Diagnostic Services",
+    "city":"Dhaka",
+    "phone":"01708519796",
+    "partnercode":"PSDD1",
+    "lat":23.7240853,
+    "lng":90.396293,
+    "address":"Hussaini Dalan Road, Opposite Dhaka Medical-2, Dhaka",
+    "discount_info" : "12% on pathology",
+    "tz":"Asia/Dhaka"
+  },
+  {
+    "name":"NEW LIFE MEDICAL CENTER",
+    "city":"Dhaka",
+    "phone":"01708519796",
+    "partnercode":"PSDD1",
+    "lat":23.7016885,
+    "lng":90.4415332,
+    "address":"Old Dania Road, Dhaka",
+    "discount_info" : "12% on pathology",
+    "tz":"Asia/Dhaka"
+  }
+
+] ;
---
+++ map_partner/maps/markers_pharmacy.js
@@ -0,0 +1,90 @@
+var markers_pharmacy = [
+  {
+    "name":"Janata pharmacy",
+    "city":"Dhaka",
+    "phone":"01716893050",
+    "partnercode":"TP130",
+    "lat":23.7720629,
+    "lng":90.3754393,
+    "address":"sample_address",
+    "discount_info" : "25% on medicine",
+    "tz":"Asia/Dhaka"
+  },
+  {
+    "name":"Medi Pharma",
+    "city":"Dhaka",
+    "phone":"0194532XXXX",
+    "partnercode":"TP017",
+    "lat":23.7892235,
+    "lng":90.3649272,
+    "address":"sample_address",
+    "discount_info" : "30% on medicine",
+    "tz":"Asia/Dhaka"
+  },
+  {
+      "name":"Pharma Aid, Dhaka",
+      "city":"Dhaka",
+      "phone":"0194582XXXX",
+      "partnercode":"TP028",
+      "lat":23.8376125,
+      "lng":90.3688108,
+      "address":"sample_address",
+      "discount_info" : "32% on medicine",
+      "tz":"Asia/Dhaka"
+  },
+  {
+      "name":"Mozumder Pharmacy",
+      "city":"Chittagong",
+      "phone":"0194582XXXX",
+      "partnercode":"TP028",
+      "lat":22.3520311,
+      "lng":91.783582,
+      "address":"Hazi Ashraf Ali Road, Chittagong",
+      "discount_info" : "32% on medicine",
+      "tz":"Asia/Dhaka"
+  },
+  {
+      "name":"Adnan Pharmacy",
+      "city":"Chittagong",
+      "phone":"0194582XXXX",
+      "partnercode":"ADNP1",
+      "lat":23.2128491,
+      "lng":90.272298,
+      "address":"Adnan Pharmacy, Chittagong",
+      "discount_info" : "24% on medicine",
+      "tz":"Asia/Dhaka"
+  },
+  {
+      "name":"Pabna Medicine Corner",
+      "city":"Pabna",
+      "phone":"0194582XXXX",
+      "partnercode":"TP134",
+      "lat":24.2172294,
+      "lng":89.3787447,
+      "address":"Pabna Medicine Corner,Bhangura, Pabna",
+      "discount_info" : "20% on medicine",
+      "tz":"Asia/Dhaka"
+  },
+  {
+      "name":"Habib Pharmacy",
+      "city":"Bagura",
+      "phone":"0194582XXXX",
+      "partnercode":"TP135",
+      "lat":24.7855811,
+      "lng":89.3018725,
+      "address":"Habib Pharmacy, bagura",
+      "discount_info" : "10% on medicine",
+      "tz":"Asia/Dhaka"
+  },
+  {
+      "name":"Family Pharma",
+      "city":"Tongi",
+      "phone":"017339836604",
+      "partnercode":"TP151",
+      "lat":23.9130611,
+      "lng":90.3941221,
+      "address":"Family Pharma, Tongi",
+      "discount_info" : "10% on medicine (except insuline)",
+      "tz":"Asia/Dhaka"
+  }
+] ;
